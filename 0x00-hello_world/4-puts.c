ad9354b86c37cf7bdfbcf109a5809fa2d2e5937e
Mrdrgh <mrdrgh2003@gmail.com>
2023-06-15 14:25:47 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
-int main() {
-puts("\"Programming is like building a multilingual puzzle");
-return 0; }
+/*
+*main , whatever 
+*/
+int main() 
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0; 
+}
