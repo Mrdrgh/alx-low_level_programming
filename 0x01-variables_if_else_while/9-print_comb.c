a38ec0d620baf03c5be58972e7b1e1ad003b104e
Mrdrgh <Mrdrgh@users.noreply.github.com>
2023-06-16 20:08:03 +0100

    mlqkskfdmqd

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - qmkfdjqmdkflfq
+ *
+ * Return: qlksmfqmdf
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putcahr(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
