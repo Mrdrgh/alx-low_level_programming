564c71fe11af57e368330106e39aa550d16fc5fd
Mrdrgh <Mrdrgh@users.noreply.github.com>
2023-06-16 20:00:57 +0100

    qdmflqkd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - qmdfkjqmsldfdjkmqdfd
+ *
+ * Return: amlqkjjefmakeza
+*/
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++
+	}
+	c = 'a';
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
