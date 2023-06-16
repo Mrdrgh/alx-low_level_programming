e905c2375e5c204f3179edb283092665527454e7
Mrdrgh <Mrdrgh@users.noreply.github.com>
2023-06-16 18:05:15 +0100

    putchar function

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.>
+/**
+ * main - print all the chars
+ *
+ * Return: Always 0(SUCCESS)
+*/
+int main(void)
+{
+	char c = 'a';
+	char b = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
