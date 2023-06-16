3361e38a5110089646078f18d424199faeb8e1f9
Mrdrgh <Mrdrgh@users.noreply.github.com>
2023-06-16 17:57:06 +0100

    putchar

---
+++ 0x01-variables_if_else_while/2-printf_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printf all the alphabet in lower case
+ *
+ * Return: Always 0(SUCCESS
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c < 'z')
+	{
+		putchar(c);
+	}
+	printf("\n");
+	return (0);
+}
