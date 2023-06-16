36427826311481fc162464bd55ae21b78498c7a5
Mrdrgh <Mrdrgh@users.noreply.github.com>
2023-06-16 20:38:08 +0100

    a hard but soft

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - qmlkfqmlfd
+ *
+ * Return: qlmkdfqmd
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i * 10 + j < j * 10 + i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i == 9 && j == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
