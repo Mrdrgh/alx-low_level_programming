550bc9469e0fc70ee5b14dbaa2447486f655523d
Mrdrgh <Mrdrgh@users.noreply.github.com>
2023-06-16 19:36:52 +0100

    if statement

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - return alphabets except p end e
+ *
+ * Return: always 0(SUCCESS)
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
