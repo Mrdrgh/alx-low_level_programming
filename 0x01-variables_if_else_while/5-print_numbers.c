bf0c7779b7047c5934118f76054768e479a35e18
Mrdrgh <Mrdrgh@users.noreply.github.com>
2023-06-16 19:45:50 +0100

    qmlkdsfjqmlksd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - return the follqdfqlmsfd
+ *
+ * Return: always 0(SUCCESS)
+*/
+int main(void)
+{
+	for (int i = 0 ; i < 10 ; i++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
