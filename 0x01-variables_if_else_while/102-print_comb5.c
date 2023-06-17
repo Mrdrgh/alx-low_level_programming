642f5813b782ba44701b89a1d783abf3743617d1
Mrdrgh <Mrdrgh@users.noreply.github.com>
2023-06-17 01:13:09 +0100

    qmldkfqfd

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,18 +9,16 @@ int main(void)
 int i;
 int j;
 
-for (i = 0 ; i < 100; i++)
+for (i = 0 ; i < 99; i++)
 {
-for (j = 0; j < 100; j++)
+for (j = i+1 ; j < 100; j++)
 {
-if (i == 0)
-	j = 1;
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-if (i < 99 || j < 99)
+if (i != 98)
 {
 putchar(',');
 putchar(' ');
