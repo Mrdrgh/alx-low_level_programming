58373d6a5da55c46fbfc407de0bc0d3ff1e037a2
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-20 12:39:32 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - mqljdfkqmlskdf
+ *
+ * Return: always 0(SUCCESS)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+fqmskdjqmkdfkqmlksdff
