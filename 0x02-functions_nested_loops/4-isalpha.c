e7b10a0e89fe5cdc91b1f947d33c30593e56efa7
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-20 18:43:03 +0100

    is alpha is like is lower with extra steps (Morty)

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isalpha - if the number is a char in ascii 
+ * _isalpha - if the number is a char in ascii
  * @c: the int to check
  * Return: 1 if its a letter, 0 if otherwise
 */
 
 int _isalpha(int c)
 {
-	if ((c <= 'z' && c >= 'a') || (c <='Z' && c >= 'A'))
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
 	{
 		return (1);
 	}
