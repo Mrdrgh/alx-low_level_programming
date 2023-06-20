37761705066c5ce73890d5d0bef7838c1da8a475
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-20 16:54:19 +0100

    header is main.h , _putchar.c contains the definition of the declaration in main.h , 0-... containsthe code running this function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - mqljdfkqmlskdf
+ * main - print putchar
  *
  * Return: always 0(SUCCESS)
 */
+
 int main(void)
 {
-	printf("_putchar\n");
-	return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
+
