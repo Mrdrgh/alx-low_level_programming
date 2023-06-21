255b7dfebe8c9cc0bc3107658b07886fe919376d
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-21 01:58:41 +0100

    hey we go

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,8 @@ void print_to_98(int n)
 	{	
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(i + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			if (i == 98)
 			{
 				break;
@@ -26,7 +27,8 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i--)
 		{
-			_putchar(i + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			if (i == 98)
 			{
 				break;
