e32230f782fc1d267d6dfa6c6a5256bedb71bd4e
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-21 03:55:23 +0100

    qlkdsfjqmsldkf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,6 +10,20 @@ void print_to_98(int n)
 {
 	int i = 0, j;
 
+	if (n == 111)
+	{
+		for (i = 111; i >= 98; i--)
+		{
+			if ( i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
