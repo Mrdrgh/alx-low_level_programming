4c982b4e61c9e445b9a46e29a0a343293a73b48c
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-21 01:43:47 +0100

    baby i'm wigged out and wigged in

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add .
+ * @a: the perimiter a
+ * @b: the perimiter b
+ * Return: returns the value of the addition of a and b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
