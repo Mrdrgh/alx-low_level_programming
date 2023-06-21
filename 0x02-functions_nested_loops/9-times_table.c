5988e730d3309a4ed1e2b88d942be4e91184b52d
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-21 01:21:45 +0100

    hope it works in the first try , if it didn't you wouldn't see this msg

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - print the multiplication table
+*/
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i == 0)
+			{
+				_putchar(i * j + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (i * j < 10)
+			{
+				_putchar(i * j + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((i * j / 10) + '0');
+				_putchar((i * j % 10) + '0');
+			}
+		}
+	}
+}
+
