7fc49e2959ec1e4a762b17bf68d38ad3be1cebd4
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-23 02:23:40 +0100

    should work pleaaase

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - returns the max prime factor of n
+ * Return: always 0
+*/
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int maximum;
+	long int i;
+
+	while (n % 2 == 0)
+	{
+		maximum = 2;
+		n = n / 2;
+	}
+	while (n % 3 == 0)
+	{
+		maximum = 3;
+		n = n / 3;
+	}
+	for (i = 5; i < sqrt(n); i = i + 2)
+		{
+			while ( n % i == 0)
+			{
+				max = i
+				n = n / i;
+			}
+		}
+	if (n > 2)
+	{
+		maximum = n;
+	}
+	printf("%ld");
+	return (0);
+}
