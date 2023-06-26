d7e58e3b7266c03ec5235bf13f1a040cd56704f2
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-26 16:49:08 +0100

    ez pz

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints an array
+ * @a: the array
+ * @n: the size of the array , in c we cant know the size of the array if we
+ * dont already have it
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n)
+		{
+			printf(", ");
+		}
+	}
+}
