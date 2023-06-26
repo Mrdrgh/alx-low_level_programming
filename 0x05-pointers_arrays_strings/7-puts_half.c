13ff3c89e17b7e5bf1ee00516abe146e66a15845
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-26 16:35:36 +0100

    hi hihihi

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - puts half of the string
+ * @str: the string to put half of it to stdout
+*/
+
+void puts_half(char *str)
+{
+	int i = 0, j;
+	char *p = str;
+
+	while ((*p))
+	{
+		p++;
+		i++;
+	}
+	i--;
+	int n = (i - 1) / 2;
+
+	for (j = n; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
