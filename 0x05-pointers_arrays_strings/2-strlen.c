52bab3f6def1b27a330eb214acc6bd9ada58980e
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-26 14:16:19 +0100

    lets see

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of an str using fseek
+ * @s: the perimiter to check
+ * Return: the size of s
+*/
+
+int _strlen(char *s)
+{
+	char *p = s;
+	int i = 0;
+
+	while (p)
+	{
+		i++;
+	}
+	return (i);
+}
