7926f9fea98f1b914cb9f5c8326127e105881f4b
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-26 16:05:12 +0100

    hurra dies velt geht unter

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: the string
+*/
+
+void rev_string(char *s)
+{
+	char *p1 = s;
+	int i = 0, j = 0;
+	char temp;
+
+	while((*p1))
+	{
+		i++;
+		p1++;
+	}
+	i--;
+	for (; i > 0, j < i; j++, i--)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
+}
