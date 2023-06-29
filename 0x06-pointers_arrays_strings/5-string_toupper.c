d712f8741c7522ac7cb1a551ea476ebb13e05e64
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-29 15:18:14 +0100

    fast and f

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - to uppercase
+ * @c: the string
+ * Return: the new to upper string
+*/
+
+char *string_toupper(char *c)
+{
+	char *p = c;
+
+	while (*p)
+	{
+		if (*p <= 'z' && *p >= 'a')
+		{
+			*p += 32;
+		}
+		p++;
+	}
+	return (c);
+}
