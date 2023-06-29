e7cbdbf98270d331969f60992668380931afaa9f
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-29 14:52:18 +0100

    should work

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 0 if the same -1 if s1 < s2 +1 if else
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	char *p = s1;
+	char *q = s2;
+
+	while (*p && *q)
+	{
+		if (*p < *q)
+		{
+			return (-1);
+		}
+		else if (*p > *q)
+		{
+			return (1);
+		}
+		p++;
+		q++;
+	}
+	return (0);
+}
