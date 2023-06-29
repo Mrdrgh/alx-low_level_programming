d08c5108be52eebfecfdb8cb3e4781ef188d7053
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-29 14:23:24 +0100

    plz work

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - cpies a string into another only the first n chars
+ * @dest: the destination
+ * @src: the source
+ * @n: the number of chars to cp
+ * Return: return the dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	char *p = src;
+	char *q = dest;
+
+	while (i < n && *p)
+	{
+		*q = *p;
+		i++;
+		p++;
+		q++;
+	}
+	*q = '\0';
+	return (dest);
+}
