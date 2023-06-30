fa65d1fd2d47272a574d09dbd725127f9db41504
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-06-30 01:19:13 +0100

    finally

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,7 +7,7 @@
 
 char *leet(char *c)
 {
-	char leet[] = {'a', 'A', 'e', 'E', 'o', 'O','t', 'T', 'l', 'L'};
+	char leet[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char decode[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 	int i, j;
 
