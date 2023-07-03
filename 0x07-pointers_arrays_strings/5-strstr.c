f889581229473ca03fe60ceb7d02c6aab754baa2
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-03 23:06:24 +0100

    should work

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,20 +3,16 @@
  * _strstr - searchs for the firs occurence of a string in a string
  * @haystack: the string to look in
  * @needle: the string to look for
- * @Return: the pointer to the substring result
+ * Return: the pointer to the substring result
 */
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *p = haystack;
-	char *q = needle;
-	int needle_len = 0, haystack_len = 0;
-	int i;
-
 	for (; *haystack != '\0'; haystack++)
 	{
-		p = haystack;
-		q = needle;
+		char *p = haystack;
+		char *q = needle;
+
 		while (*p == *q && *q != '\0')
 		{
 			p++;
