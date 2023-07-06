b3a2fa81650a4cee121649e8d9d2e19e6db7f3ce
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-06 08:36:51 +0100

    should work

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts_recursion - puts a string recursivly
+ * _print_rev_recursion - puts a string recursivly
  * @s: the string
 */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (!*s)
 	{
 		_putchar('\n');
 		return;
 	}
-	_puts_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(s[0]);
 }
