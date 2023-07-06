8509b9c0c19933c194b30439b7350d53b1389738
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-06 21:26:22 +0100

    hard as fuck

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * is_prime_number - checks if n is prime , really tricky
+ * @n: the perimiter
+ * Return: 1 if true , 0 else
+*/
+
+int is_prime_number(int n)
+{
+	int p;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (n == 2) ? 1 : 0 ;
+	}
+	for (p = 3; p * p < n; p += 2)
+	{
+		if (is_prime_number(p) && n % p == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
