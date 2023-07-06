3d4c041ac4e7322d1d9a9de43a58be71f6d16d64
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-06 21:03:48 +0100

    should work

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of in int
+ * @n: the int
+ * Return: the result
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (is_natural(0, n));
+}
+/**
+ * is_natural - sees if natural exists
+ * @n: the number
+ * @i: iteration tool
+ * Return: the result
+*/
+
+int is_natural(int i, int n)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (is_natural(i + 1, n));
+}
