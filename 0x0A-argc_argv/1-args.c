7e6a0e981bb6aa61e0dd88e2937fdb3ec21a2416
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-07 13:04:53 +0100

    should work

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of args
+ * @argc: the number of args
+ * @argv: the args
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
