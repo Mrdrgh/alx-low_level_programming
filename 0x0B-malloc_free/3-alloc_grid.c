d1c9a9984104364a60d3eb6e606256f782e94fdc
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-10 20:02:13 +0100

    should work now

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * alloc_grid - allocate a mem to a 2 dims grid
+ * @width: self explanatory
+ * @height: whatever
+ * Return: the pointer to the grid
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i = 0;
+	int j = 0;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = (int **) malloc(sizeof(int *) * width);
+	if (!grid)
+		return (NULL);
+	for (; i < width; i++)
+	{
+		grid[i] = (int *) malloc(sizeof(int) * height);
+		if (!grid)
+			return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			grid[i] + j = 0;
+		}
+	}
+	return (grid);
+}
+
