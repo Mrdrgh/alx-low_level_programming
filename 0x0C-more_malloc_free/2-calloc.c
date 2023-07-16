885e3b8e8804d4f293b456812792511e63be0e03
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-16 17:57:50 +0100

    lets see hmmm

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _calloc - like maloc but with zero s 
+ * @nmemb: the number of elements to allocate memo for
+ * @size: the size of each element
+ * Return: a pointer of type void to this chunk of mem
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	char *p;
+
+	if (nmemb <= 0)
+		return (NULL);
+	*ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	p = ptr;
+	while (p)
+	{
+		*p = 0;
+		p++;
+	}
+	return (ptr);
+}
+	
