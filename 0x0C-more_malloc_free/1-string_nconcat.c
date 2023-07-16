2d019d879bf5e020bc9d78618182bfbc4efd2e09
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-16 16:10:34 +0100

    should work

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * string_nconcat - concats the n chars of s2 to the end of s1
+ * @s1: the firsst string
+ * @s2: the second one
+ * @n: the number of chars
+ * Return: the newly created string
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len = 0, i = 0;
+	char *p1 = s1, *p2 = s2;
+	char *str;
+
+	while (*p1++)
+		len1++;
+
+	while (*p2++ && i++ < n)
+		len1++;
+	str = malloc(sizeof(char) * (len1) + 1);
+	if (!str)
+		return (NULL);
+	p1 = s1;
+	p2 = s2;
+	i = 0;
+	while (*p1++)
+	{
+		str[i++] = *p1;
+	}
+	while (*p2++ && i < len1 + n)
+		str[i++] = *p2;
+	str[i] = '\0';
+	return (str);
+}
