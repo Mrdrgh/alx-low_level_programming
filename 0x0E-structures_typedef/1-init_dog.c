cb39b839b539c2c4a4ab9642d4788e0243cb5422
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-17 14:11:56 +0100

    should work

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+*init_dog - itializes a struct
+*@d: a struct of dog
+*@name: the name of dog
+*@age: the age
+*@owner: the owner
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (!d)
+		d = malloc(sizeof(struct dog));
+
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
