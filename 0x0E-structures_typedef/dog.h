c94a6c94116fc2f77720399123aaecefa8ee510a
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-17 13:55:58 +0100

    should work

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - a struct of dog
+ *@name: the name
+ *@age: the age
+ *@owner: the owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
