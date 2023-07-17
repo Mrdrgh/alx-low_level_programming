32fa3932667b6027cdd9d61b88bb82fe2424ae51
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-17 14:34:38 +0100

    shoul wmqlkjdff

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,17 +7,20 @@
 void print_dog(struct dog *d)
 {
 	if (!d)
-		exit(98);
+		return;	
 	printf("Name: ");
 	if (d->name == NULL)
-		printf("nil\n");
+		printf("(nil)\n");
 	else
 		printf("%s\n", d->name);
 	printf("Age: ");
-	printf("%f\n", d->age);
+	if (d->age == 0)
+		printf("(nil)\n");
+	else
+		printf("%f\n", d->age);
 	printf("Owner: ");
 	if (d->owner == NULL)
-		printf("nil\n");
+		printf("(nil)\n");
 	else
 		printf("%s\n", d->owner);
 }
