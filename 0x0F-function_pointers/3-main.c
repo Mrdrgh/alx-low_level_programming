7e125c7d06bf0ac4236bf03d312cb16e00d0004a
mr_drgh <mrdrgh2003@gmail.com>
2023-07-19 21:05:20 +0100

    should work

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,13 +4,13 @@
 
 /**
  * main - if arg 2 is an operator do something else print Error
+ * Return: fmqlksdsjfmlqkjsdf
 */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
 	int arg1num, arg2num;
-	char *oop;
+	char *oop = malloc (10);
 
 	arg1num = atoi(argv[1]);
 	arg2num = atoi(argv[3]);
@@ -22,17 +22,18 @@ void main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (arg2num == 0 && oop[0] == '/' || arg1num == 0 && oop[0] == '%')
+	if ((arg2num == 0 && oop[0] == '/') || (arg1num == 0 && oop[0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	if (oop[1] != '+' && oop[1] != '-'
-	&& oop[1] =! '/' && oop[1] != '%' && oop[1] != '*')
+	if (oop[0] != '+' && oop[0] != '-'
+	&& oop[0] != '/' && oop[0] != '%' && oop[0] != '*')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	printf("%d\n",get_op_func(oop)(arg1num, arg2num));
+	return (0);
 }
 
