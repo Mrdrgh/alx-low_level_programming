f8719adf9cfded2ef9f273e73ede917c37f8906e
darghal mohammed <mrdrgh@pop-os.localdomain>
2023-07-19 12:42:18 +0100

    should work

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - applies the function action to each case of an array
+ * @array: the array of ints
+ * @size: the size of the array , in c the size is always pre defined
+ * @action: the function to apply
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
