cf7dfe0c9d219172be7dfc90ca60a9864026f9ec
mr_drgh <mrdrgh2003@gmail.com>
2023-07-27 14:09:12 +0100

    lets see

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: the list to free
+*/
+void free_list(list_t *head)
+{
+	list_t *curr = head, *temp;
+
+	temp = curr;
+	while (curr->next)
+	{
+		temp = curr;
+		curr = curr->next;
+		temp = NULL;
+		free(temp);
+	}
+	free (curr);
+	free (head);
+}
