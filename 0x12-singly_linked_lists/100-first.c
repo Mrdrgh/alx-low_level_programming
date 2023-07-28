5d2a7b5bf2fffc88c2da036332f2671df6df7e0d
mr_drgh <mrdrgh2003@gmail.com>
2023-07-28 01:36:25 +0100

    lets see

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * first - prints what is in first before executing the main
+*/
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
