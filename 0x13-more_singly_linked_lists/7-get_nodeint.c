82d8163745ac9e4f7ef258c85fce162465f64006
mr_drgh <mrdrgh2003@gmail.com>
2023-07-31 14:11:13 +0100

    works like butter

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * get_nodint_at_index - gets the nth indexed node
+ * @head: the list
+ * @index: the index of the node to get
+ * Return: the address of the returned node, if not existing NULL
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	while (head && index)
+	{
+		index--;
+		head = head->next;
+	}
+	return (head);
+}
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * sum_listint - sum
+ * @head: the list
+ * Return: the sum
+*/
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
