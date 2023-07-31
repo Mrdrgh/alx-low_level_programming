25216616c72c46f9f0e1a0d6d4ab749f91d32222
mr_drgh <mrdrgh2003@gmail.com>
2023-07-31 12:45:54 +0100

    ez pz

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - frees a list of ints
+ * @head: the list
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *temp, *curr = head;
+
+	while (curr)
+	{
+		temp = curr;
+		curr = curr->next;
+		free(temp);
+	}
+}
