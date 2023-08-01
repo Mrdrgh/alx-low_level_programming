b81101f290c10736f8fc1afb8d10821f56e8119b
mr_drgh <mrdrgh2003@gmail.com>
2023-08-01 13:46:47 +0100

    pushed 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a list of nodes
+ * @head: the list
+ * Return: the number of nodes in the list
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current = head;
+	size_t i = 0;
+
+	if (!head)
+	{
+		exit(98);
+	}
+	while (!current)
+	{
+		i++;
+		printf("[%p] %d\n", current, current->n);
+		current = current->next;
+	}
+	return (i);
+}
