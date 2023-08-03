213e71eb89bfdae81cce5d6aa410d445f43276ee
mr_drgh <mrdrgh2003@gmail.com>
2023-08-03 13:23:31 +0100

    didn't understand why exactly

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * clear_bit - set a given bit at 0
+ * @n: the number to set the bit of
+ * @index: the index
+ * Return: 1 if worked, 0 if none
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n = *n & (1UL << index);
+	return (1);
+}
