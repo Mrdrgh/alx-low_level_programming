3afe517d24930177e8d1dd70892811d42d9e316b
mr_drgh <mrdrgh2003@gmail.com>
2023-08-07 21:44:15 +0100

    fluc it

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (length = 0; text_content[length];)
 			length++;
 	}
-	chars_printed = write(file_descriptor, text_content, length - 1);
+	chars_printed = write(file_descriptor, text_content, length);
 	if (chars_printed == -1 || file_descriptor == -1)
 		return (-1);
 	close(file_descriptor);
