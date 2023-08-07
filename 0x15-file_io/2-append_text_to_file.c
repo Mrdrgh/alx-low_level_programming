17661ae7089c59d47559fabcaaadf0a9e4cb5806
mr_drgh <mrdrgh2003@gmail.com>
2023-08-07 21:39:14 +0100

    fixed edge casing for fd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,15 +13,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	file_descriptor = open(filename, O_APPEND | O_WRONLY);
-	if (file_descriptor == -1)
-		return (-1);
 	if (text_content)
 	{
 		for (length = 0; text_content[length];)
 			length++;
 	}
 	chars_printed = write(file_descriptor, text_content, length);
-	if (chars_printed == -1)
+	if (chars_printed == -1 || file_descriptor == -1)
 		return (-1);
 	close(file_descriptor);
 	return (1);
