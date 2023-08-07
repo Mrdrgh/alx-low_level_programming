af6b054c0f9edb961bd24352440fe75643f2a08d
mr_drgh <mrdrgh2003@gmail.com>
2023-08-07 17:01:47 +0100

    fixed the filedesc and the filename edge cases

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,12 +11,13 @@ int create_file(const char *filename, char *text_content)
 	ssize_t file_desc, length, char_written;
 
 	file_desc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file_desc == -1)
+	if (file_desc == -1 || !filename)
 		return (-1);
-	if (!text_content)
-		return (-1);
-	for (length = 0; text_content[length];)
-		length++;
+	if (text_content)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
 	char_written = write(file_desc, text_content, length);
 	if (char_written == -1)
 		return (-1);
