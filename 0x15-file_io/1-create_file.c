f7b62a19beea2f0ccd4c9a6b1652c6a4fda0e321
mr_drgh <mrdrgh2003@gmail.com>
2023-08-07 17:10:20 +0100

    fixed the openning modes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,19 +8,25 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t file_desc, length, char_written;
+	int fd, w, len = 0;
 
-	file_desc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file_desc == -1 || !filename)
+	if (filename == NULL)
 		return (-1);
-	if (text_content)
+
+	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
-			length++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	char_written = write(file_desc, text_content, length);
-	if (char_written == -1)
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
 		return (-1);
-	close(file_desc);
+
+	close(fd);
+
 	return (1);
 }
+
