d0c9f4755db38da392d2a595b0754ba649696f3b
mr_drgh <mrdrgh2003@gmail.com>
2023-08-07 16:22:53 +0100

    lets see

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * read_textfile - reads a file stream into the stdout
+ * @filename: the name of the file
+ * @letters: the number of the letters to output
+ * Return: the number of chars printed if secceeded
+ * 0 if filename if NULL or if write fails or filename cannot be oppened
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t printed_chars, file_descriptor;
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (-1);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+	read(file_descriptor, buffer, letters);
+	printed_chars = write(STDOUT_FILENO, buffer, letters);
+	close(file_descriptor);
+	free(buffer);
+	return (printed_chars);
+}
