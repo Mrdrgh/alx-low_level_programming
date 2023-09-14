5b3c34763df90c55cc7c5395af2c898c5e33c0c3
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-14 17:37:34 +0100

    i know it had NO FLAWS

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a double node in the head of the list
+ * @head: the supposed head of the dlist, we'll edge case on it
+ * @n: the value of the new node
+ *
+ * Return: the address of the newly created dnode, or null if fail
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *current, *new_dnode = malloc(sizeof(dlistint_t));
+
+	if (!new_dnode)
+		return (NULL);
+
+	new_dnode->n = n;
+	new_dnode->prev = NULL;
+
+	if (!*head)
+	{
+		new_dnode->next = NULL;
+		return (new_dnode);
+	}
+	while ((*head)->prev)
+		head = &(*head)->prev;
+	(*head)->prev = new_dnode;
+	new_dnode->next = (*head);
+	return (new_dnode);
+}
