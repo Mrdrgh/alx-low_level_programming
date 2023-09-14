f69a70d37c881a661a8197444b45b3d26d807c75
Mrdrgh <mrdrgh2003@gmail.com>
2023-09-15 00:37:45 +0100

    good to go

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,32 +10,45 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current, *temp, *new_dnode = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
-	if (!new_dnode)
-		return (NULL);
-	if (!*h && idx)
-		return (NULL);
-	if (!*h)
+	new = NULL;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
 	{
-		free(new_dnode);
-		return (add_dnodeint(&h, n));
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
 
-	new_dnode->n = n;
-	while ((*h)->prev)
-		*h = (*h)->prev;
-	current = *h;
-	while (current && idx)
-	{
-		temp = current;
-		current = current->next;
-	}
-	if (!current && idx)
-		return (NULL);
-	temp->next = new_dnode;
-	new_dnode->prev = temp;
-	current->prev = new_dnode;
-	new_dnode->next = current;
-	return (new_dnode);
+	return (new);
 }
+
