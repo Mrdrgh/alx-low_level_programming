18cc51bc066774fb2948d78fbe6058f5b122032c
Mrdrgh <mrdrgh2003@gmail.com>
2023-10-06 20:16:35 +0100

    fixed some regs

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,7 @@
  * Return: returns the set index
 */
 
-unsigned long int hash_index(const unsigned char *key, unsigned long int size)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
